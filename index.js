267eb56023cbb4911a57ad0d5b8dff690b6f7f6f
Arsh2k5 <arsh2005in@gmail.com>
2025-10-03 20:03:48 +0530

    Done

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.post("/chat", async (req, res) => {
           animation: "Angry",
         },
         {
-          text: "You don't want to ruin Wawa Sensei with a crazy ChatGPT and ElevenLabs bill, right?",
+          text: "You don't want to ruin Robovision with a crazy ChatGPT and ElevenLabs bill, right?",
           audio: await audioFileToBase64("audios/api_1.wav"),
           lipsync: await readJsonTranscript("audios/api_1.json"),
           facialExpression: "smile",
